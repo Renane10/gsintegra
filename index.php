959e2ebcab682ed65d262d33a89983984e5f81f0
Renan Gomes <renanoliveirae10@gmail.com>
2024-09-07 00:27:47 -0300

    Retirando, usado para debug

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-// Exibir todos os erros e avisos
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/db.php'; // Inclui a conexão com o banco de dados
